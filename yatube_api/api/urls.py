7024da6b077beb91eefc8b5bcc6d481fc72e7db9
Egor Avdeev <dist.avdeev2004@gmail.com>
2025-03-14 23:50:09 +0700

    fix

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+from rest_framework.authtoken import views
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+]
--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -1,31 +1,7 @@
+from django.contrib import admin
 from django.urls import include, path
-from rest_framework.authtoken import views
-from rest_framework.routers import SimpleRouter
-from api.views import GroupViewSet, PostViewSet, CommentViewSet
 
-router = SimpleRouter()
-router.register('groups', GroupViewSet, basename='groups')
-router.register('posts', PostViewSet, basename='posts')
-# Create your views here.
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/', include(router.urls)),
-    path(
-        'api/v1/posts/<int:post_id>/comments/',
-        CommentViewSet.as_view({
-            'get': 'list',
-            'post': 'create'
-        }),
-        name='comment-list'
-    ),
-    path(
-        'api/v1/posts/<int:post_id>/comments/<int:pk>/',
-        CommentViewSet.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'patch': 'partial_update',
-            'delete': 'destroy'
-        }),
-        name='comment-detail'
-    ),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
