7e807e385198d16a0c2197764cb686ff8dbdf1aa
Egor Avdeev <dist.avdeev2004@gmail.com>
2025-03-14 23:52:12 +0700

    fix

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,7 +6,11 @@ from rest_framework.authtoken import views
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
