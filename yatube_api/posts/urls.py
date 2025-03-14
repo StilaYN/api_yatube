e7b54166650566f3287c1e78c57c6d2127afcb8e
Egor Avdeev <dist.avdeev2004@gmail.com>
2025-03-14 21:58:42 +0700

    Исправил полржение в файлах

--- yatube_api/posts/urls.py
+++ yatube_api/posts/urls.py
@@ -1,4 +1,9 @@
+from django.shortcuts import render
+from rest_framework.authtoken import views
 from django.urls import path
 
+# Create your views here.
 urlpatterns = [
+    path('api/v1/api-token-auth/', views.obtain_auth_token)
 ]
+
